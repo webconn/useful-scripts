c4ece75e7cbbdf2af14b6ddbd9ffd0ebaf927ed9
Nikita webconn Maslov <wkernelteam@gmail.com>
2017-01-10 15:24:37 +0300

    add Golang backtrace-by-pid script
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

---
+++ golang/backtrace-by-pid.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+GOPID=$1
+TEMPFILE=`tempfile`
+
+# run strace
+strace -e write -p $GOPID -o $TEMPFILE & STRACE_PID=$!
+
+# TODO: wait for strace to be attached more properly
+sleep 5
+
+# send SIGQUIT to process
+kill -QUIT $GOPID
+
+# wait for strace to quit
+echo "Waiting for strace to be done..."
+
+wait $STRACE_PID
+
+# print formatted data
+cat $TEMPFILE | sed -n 's/write(2, \"\(.*\)\".*$/\1/p' | tr -d '\n' | sed 's/\\n/\n/g' | sed 's/\\t/\t/g'
+
+# remove temp file
+rm $TEMPFILE
